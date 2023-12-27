7a9047ce539ce4cfd491900b9b9d508cf3db75f5
darwinestiven <dd5147609@gmail.com>
2023-12-27 17:38:04 -0500

    Commit Portada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
     <div class="container">
-        <h1>Proyecto Estudiantes Laravel</h1>
+        <h1 class="mt-5 text-primary">Proyecto Estudiantes Laravel</h1>
+        <div class="card mt-4">
+            <div class="card-body">
+                <h2 class="card-title">Realizado por:</h2>
+                <p class="card-text">Darwin Estiven Diaz Espinosa</p>
+                <p class="card-text">Código: 219036069</p>
+                <p class="card-text">Celular: 3116282163</p>
+            </div>
+        </div>
     </div>  
 @endsection
